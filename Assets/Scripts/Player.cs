6c64cf57ec98d44b6a554ca1ce2585d782cbaec9
walt2 <walter22r@icloud.com>
2018-07-14 20:37:26 -0500

    Flip the player as where it has been moved

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D myRgidBody;
+
+	// Use this for initialization
+	void Start () {
+        myRgidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+        FlipSprite();
+	}
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRgidBody.velocity.y);
+        myRgidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs (myRgidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRgidBody.velocity.x), 1f);
+        }
+
+    }
+}
